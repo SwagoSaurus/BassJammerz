cd13721d3d3125297b40e1a2a5eadbdc5f79ccf4
Isklett <isak.lagerqvist@gmail.com>
2020-03-05 22:16:35 +0100

    1.0.5

--- BassJammerz/Assets/IsakFolder/Scripts/DeathScript.cs
+++ BassJammerz/Assets/IsakFolder/Scripts/DeathScript.cs
@@ -26,6 +26,7 @@ public class DeathScript : MonoBehaviour
         if (col.gameObject.tag == "Note")
         {
             Script.ResetCombo();
+            Script.ResetStreak();
             Destroy(col.gameObject);
         }
     }
